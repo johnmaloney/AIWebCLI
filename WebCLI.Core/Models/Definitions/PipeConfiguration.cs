224fe13097a6faf44a7f6aeb5c80527eb85cc647
John Maloney <maloney1@gmail.com>
2025-12-02 15:35:52 -0700

    feat: Add PipeConfiguration model for JSON pipeline definitions

---
+++ WebCLI.Core/Models/Definitions/PipeConfiguration.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCLI.Core.Models.Definitions
+{
+    public class PipeConfiguration
+    {
+        public string Type { get; set; }
+        public string Assembly { get; set; }
+        public string Namespace { get; set; }
+        public string ContextType { get; set; }
+        // Add any additional configuration properties needed for a pipe here
+    }
+}
