f2566ec9a730beaa3885875eea2d1a7a54366a87
johnmaloney <maloney1@gmail.com>
2025-12-02 16:40:23 -0700

    commit

--- WebCLI.Core/Contracts/EnvironmentalContext.cs
+++ WebCLI.Core/Contracts/EnvironmentalContext.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using WCLI.Core.Contracts;
+using WebCLI.Core.Contracts;
 
 namespace WebCLI.Core.Contracts
 {
