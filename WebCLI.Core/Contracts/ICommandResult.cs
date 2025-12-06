e7e6c3d7bfd418559423a6dee8afeaf925db3e27
John Maloney <maloney1@gmail.com>
2025-12-05 20:38:23 -0700

    Fix: Added missing properties to ICommandResult interface.

--- WebCLI.Core/Contracts/ICommandResult.cs
+++ WebCLI.Core/Contracts/ICommandResult.cs
@@ -1,6 +1,12 @@
-﻿namespace WebCLI.Core.Contracts
+using System.Collections.Generic;
+
+namespace WebCLI.Core.Contracts
 {
     public interface ICommandResult
     {
+        bool Success { get; set; }
+        List<string> Messages { get; set; }
+        object Response { get; set; }
+        string ResponseType { get; set; }
     }
 }
