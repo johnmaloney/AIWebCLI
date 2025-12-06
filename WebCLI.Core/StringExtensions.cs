c5bcbe587db7d7f943ebf50c9a923509970b729c
John Maloney <maloney1@gmail.com>
2025-12-05 18:48:13 -0700

    fix: Update RightSideOf to handle multiple split characters correctly

--- WebCLI.Core/StringExtensions.cs
+++ WebCLI.Core/StringExtensions.cs
@@ -17,8 +17,8 @@ namespace WebCLI.Core
         /// <returns></returns>
         public static string RightSideOf(this string value, string splitCharacter)
         {
-            var values = Regex.Split(value, splitCharacter);
-            return values.Skip(1).First();
+            var values = value.Split(new[] { splitCharacter }, StringSplitOptions.None);
+            return string.Join(splitCharacter, values.Skip(1));
         }
 
         /// <summary>
@@ -29,7 +29,7 @@ namespace WebCLI.Core
         /// <returns></returns>
         public static string LeftSideOf(this string value, string splitCharacter)
         {
-            var values = Regex.Split(value, splitCharacter);
+            var values = value.Split(new[] { splitCharacter }, StringSplitOptions.None);
             return values.First();
         }
 
@@ -44,13 +44,11 @@ namespace WebCLI.Core
             if (item == null) return false;
             if (string.IsNullOrEmpty(nodeName)) return false;
 
-            // Safely cast to IDictionary<string, object> to check for ContainsKey
             if (item is IDictionary<string, object> dictionaryItem)
             {
                 return dictionaryItem.ContainsKey(nodeName);
             }
             
-            // If it's not a dictionary-like dynamic object, assume it doesn't have the node
             return false;
         }
 
@@ -67,8 +65,6 @@ namespace WebCLI.Core
 
             try
             {
-                // The original logic was inverted. It should return true if the item at the location is NOT null.
-                // And false if it IS null or an exception occurs (e.g., out of bounds).
                 return item[location] != null;
             }
             catch (Exception)
