429ff9dc7aef4aeb18e2289cc6eb9eac5dd1656e
John Maloney <maloney1@gmail.com>
2025-12-06 13:25:54 -0700

    Add QueryDocumentation class

---
+++ WebCLI.Core/Documentation/QueryDocumentation.cs
@@ -0,0 +1,13 @@
+using WebCLI.Core.Queries;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebCLI.Core.Documentation
+{
+    public class QueryDocumentation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<string> Parameters { get; set; } = new List<string>();
+    }
+}
