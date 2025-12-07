67dab745f930c10e6b3725788b373601acd5e7e4
John Maloney <maloney1@gmail.com>
2025-12-07 10:46:55 -0700

    feat: Update IPipelineInitializer to match DynamicPipelineInitializer

--- WebCLI.Core/Contracts/IPipelineInitializer.cs
+++ WebCLI.Core/Contracts/IPipelineInitializer.cs
@@ -1,11 +1,12 @@
 using WebCLI.Core.Models;
+using System.Collections.Generic; // Added for IEnumerable
 
 namespace WebCLI.Core.Contracts
 {
     public interface IPipelineInitializer
     {
         Task<CommandResult> ExecuteCommandPipeline(ICommand command);
-        Task<TResult> ExecuteQueryPipeline<TResult> (IQuery<TResult> query);
-        IEnumerable<PipelineDefinition> GetAllPipelineDefinitions(); // New method
+        Task<TResult> ExecuteQueryPipeline<TResult>(IQuery<TResult> query);
+        IEnumerable<PipelineDefinition> GetAllPipelineDefinitions();
     }
 }
