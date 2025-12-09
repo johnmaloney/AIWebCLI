f1278847f4bb526e28d269dcbd4701da03b714f5
John Maloney <maloney1@gmail.com>
2025-12-09 11:19:30 -0700

    feat: Update AdminController to use IPipelineInitializer for active pipelines

--- WebCLI.API/Controllers/AdminController.cs
+++ WebCLI.API/Controllers/AdminController.cs
@@ -10,23 +10,25 @@ namespace WebCLI.API.Controllers
     public class AdminController : ControllerBase
     {
         private readonly IPipelineDefinitionRepository _pipelineDefinitionRepository;
+        private readonly IPipelineInitializer _pipelineInitializer;
 
-        public AdminController(IPipelineDefinitionRepository pipelineDefinitionRepository)
+        public AdminController(IPipelineDefinitionRepository pipelineDefinitionRepository, IPipelineInitializer pipelineInitializer)
         {
             _pipelineDefinitionRepository = pipelineDefinitionRepository;
+            _pipelineInitializer = pipelineInitializer;
         }
 
         [HttpGet("pipelines")]
         public ActionResult<IEnumerable<PipelineDefinition>> GetAllPipelineDefinitions()
         {
-            var definitions = _pipelineDefinitionRepository.GetAllPipelineDefinitions();
+            var definitions = _pipelineInitializer.GetAllPipelineDefinitions();
             return Ok(definitions);
         }
 
         [HttpGet("pipelines/{name}")]
         public ActionResult<PipelineDefinition> GetPipelineDefinition(string name)
         {
-            var definition = _pipelineDefinitionRepository.GetPipelineDefinition(name);
+            var definition = _pipelineInitializer.GetAllPipelineDefinitions().FirstOrDefault(p => p.Name == name); // Modified to use _pipelineInitializer
             if (definition == null)
             {
                 return NotFound();
