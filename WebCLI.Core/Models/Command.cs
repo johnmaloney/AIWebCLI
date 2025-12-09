e4039a342f48646796b7ee8bfba817098c0c7c5f
John Maloney <maloney1@gmail.com>
2025-12-09 09:10:25 -0700

    Implement ICommand.Options in Command class

--- WebCLI.Core/Models/Command.cs
+++ WebCLI.Core/Models/Command.cs
@@ -6,11 +6,13 @@ namespace WebCLI.Core.Models
     {
         public string Name { get; }
         public Dictionary<string, string> Parameters { get; }
+        public Dictionary<string, string> Options { get; }
 
-        public Command(string name, Dictionary<string, string> parameters = null)
+        public Command(string name, Dictionary<string, string> parameters = null, Dictionary<string, string> options = null)
         {
             Name = name;
             Parameters = parameters ?? new Dictionary<string, string>();
+            Options = options ?? new Dictionary<string, string>();
         }
     }
 }
