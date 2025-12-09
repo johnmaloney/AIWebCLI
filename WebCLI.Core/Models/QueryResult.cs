3aa6976a4ccbd3b319dfc6bd0811ba599c7988d0
John Maloney <maloney1@gmail.com>
2025-12-09 09:14:03 -0700

    Update QueryResult to reference Contracts.IQueryResult

--- WebCLI.Core/Models/QueryResult.cs
+++ WebCLI.Core/Models/QueryResult.cs
@@ -1,9 +1,9 @@
 using System.Collections.Generic;
-using WebCLI.Core.Contracts;
+using WebCLI.Core.Contracts; // Ensure this using directive is present
 
 namespace WebCLI.Core.Models
 {
-    public class QueryResult<T> : IQueryResult
+    public class QueryResult<T> : WebCLI.Core.Contracts.IQueryResult // Explicitly reference the Contracts version
     {
         public bool Success { get; set; }
         public List<string> Messages { get; set; } = new List<string>();
@@ -24,7 +24,7 @@ namespace WebCLI.Core.Models
         }
 
         // Explicit interface implementation for non-generic IQueryResult
-        object IQueryResult.Data
+        object WebCLI.Core.Contracts.IQueryResult.Data // Explicitly reference the Contracts version
         {
             get => Data;
             set => Data = (T)value; // Add the setter and cast to T
