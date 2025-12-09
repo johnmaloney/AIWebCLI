1d028a67680795cddfa20c696a7496dd6ada0789
John Maloney <maloney1@gmail.com>
2025-12-09 10:28:28 -0700

    Removed duplicate PipeDetailsDto class definition from PipelineDetailsDto.cs

--- WebCLI.API/Models/PipelineDetailsDto.cs
+++ WebCLI.API/Models/PipelineDetailsDto.cs
@@ -1,14 +1,5 @@
 namespace WebCLI.API.Models
 {
-    public class PipeDetailsDto
-    {
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public string InputType { get; set; }
-        public string OutputType { get; set; }
-        public Dictionary<string, string> Parameters { get; set; } // ParameterName -> Type/Description
-    }
-
     public class PipelineDetailsDto
     {
         public string Name { get; set; }
